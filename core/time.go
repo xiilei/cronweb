e225d58ec57294d1873f928fe03c1a7c2bd093d9
xilei <xilei125@163.com>
2015-08-03 22:25:44 +0800

    let month_days as global variables

--- core/time.go
+++ core/time.go
@@ -29,12 +29,13 @@ var Months = [...]string{
 	"Dec",
 }
 
+var month_days = [...]int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+
 //DaysInMonth return total days of the month in year
 func DaysInMonth(month, year int) int {
 	month = month - 1
-	days := [...]int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 	if month == 1 && ((year%4 == 0 && year%100 != 0) || year%400 == 0) {
 		return 29
 	}
-	return days[month]
+	return month_days[month]
 }
